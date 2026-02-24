68a4fec1fdf6e4cbd1fe016f51994c29ca9b27dc
vijayasankar <vijayasankar2225@gmail.com>
2026-02-24 10:30:48 +0530

    Object class added

--- src/ObjectClass.java
+++ src/ObjectClass.java
@@ -1,2 +1,23 @@
 public class ObjectClass {
+
+int num;
+
+    public ObjectClass(int num) {
+        this.num = num;
+    }
+
+    public int hashCode() {
+        return num + 24;
+    }
+    public boolean equals(Object obj) {
+        return obj == this;
+    }
+
+    public static void main(String[] args) {
+        ObjectClass obj1 = new ObjectClass(15);
+        ObjectClass obj2 = new ObjectClass(2);
+        System.out.println(obj1.hashCode());
+
+    }
 }
+
