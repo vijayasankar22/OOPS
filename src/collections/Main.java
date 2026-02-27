b12ddbceac0caea5f49cd951f07fbc1d2a6bb1b8
vijayasankar <vijayasankar2225@gmail.com>
2026-02-27 15:43:08 +0530

    Get Classs

---
+++ src/collections/Main.java
@@ -0,0 +1,17 @@
+package collections;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+
+public class Main {
+    public static void main(String[] args) {
+        Collection collection = new ArrayList();
+        collection.add("a");
+        collection.add("b");
+
+        System.out.println(collection);
+
+
+    }
+}
---
+++ src/encapsulation/B.java
@@ -0,0 +1,9 @@
+package encapsulation;
+
+public class B {
+    public static void main(String[] args) {
+
+        A as = new A("John");
+        System.out.println(as.name);
+    }
+}
