8a4d347f055947f962b413a5321f08b4486bf070
Vijayasankar <vijayasankar2225@gmail.com>
2026-03-06 12:05:13 +0530

    Biitwise Package Created

---
+++ src/bitwiseoperators/OperationsBit.java
@@ -0,0 +1,13 @@
+package bitwiseoperators;
+
+public class OperationsBit {
+
+    public static void main(String[] args) {
+
+        int a =34;
+        int b=56;
+
+        System.out.println(a&b);
+    }
+
+}
